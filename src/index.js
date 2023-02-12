4ef16778ab4d90a20f274d2865c7b6fb79bebe5d
lecaovan2000 <lecaovan12102000@gmail.com>
2023-02-13 00:05:03 +0700

    provider store for app

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ import reportWebVitals from "./reportWebVitals";
 import "./assets/boxicons-2.0.7/css/boxicons.min.css";
 import Layout from "./components/Layout";
 import "./scss/index.scss";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Layout />
+    <Provider store={store}>
+      <Layout />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
